6bffb10ec35ce1d9219ffce5925393c4254a669a
Anurag Patil <aspanurag@gmail.com>
2017-06-05 11:47:01 -0400

    argument address

--- newCourse/weather-app/app.js
+++ newCourse/weather-app/app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        address: {
+            demand: true,
+            alias: 'a',
+            describe: 'Enter the Address to find weather',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv.address);
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true //creates own header in the response object
 }, (error, response, body) => {
     //console.log(JSON.stringify(body, undefined, 2)); //to pretty print a object
