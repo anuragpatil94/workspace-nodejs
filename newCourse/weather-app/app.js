35f3fcad325a8101befedc74a0f5b9af4656c8e2
Anurag Patil <aspanurag@gmail.com>
2017-06-05 11:19:25 -0400

    datascraping from googleapis

--- newCourse/weather-app/app.js
+++ newCourse/weather-app/app.js
@@ -2,7 +2,11 @@ const request = require('request');
 
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-    json: true
+    json: true //creates own header in the response object
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2)); //to pretty print a object
+    //console.log(JSON.stringify(body, undefined, 2)); //to pretty print a object
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude:${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude:${body.results[0].geometry.location.lng}`);
+
 });
