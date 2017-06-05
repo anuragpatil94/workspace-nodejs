537395eb96a3963746699463825ac13b64773a94
Anurag Patil <aspanurag@gmail.com>
2017-06-05 09:34:40 -0400

    async-basics

---
+++ newCourse/weather-app/playground/async-basics.js
@@ -0,0 +1,14 @@
+console.log('Starting app....');
+
+//callback function as 1st param.
+setTimeout(() => {
+    console.log('Inside of Callback 1');
+
+},2000);
+setTimeout(() => {
+    console.log('Inside of Callback 2');
+
+},0);
+
+console.log('Finishing up...');
+
