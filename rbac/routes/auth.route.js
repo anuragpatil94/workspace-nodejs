cc44a0ef0ddeeea2ba3cba6e656cfdb2cfcefc2c
Anurag Patil <aspanurag@gmail.com>
2022-02-12 16:11:05 -0500

    Add Validation for Register route

--- rbac/routes/auth.route.js
+++ rbac/routes/auth.route.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { body, validationResult } = require('express-validator')
 
 const User = require('../models/user.model')
 
@@ -18,23 +19,54 @@ router.get('/register', async (req, res, next) => {
   res.render('register')
 })
 
-router.post('/register', async (req, res, next) => {
-  try {
-    const { email } = req.body
-    const doesUserExist = await User.findOne({ email })
+router.post(
+  '/register',
+  [
+    body('email')
+      .trim()
+      .isEmail()
+      .withMessage('Email must be a valid email')
+      .normalizeEmail()
+      .toLowerCase(),
+    body('password')
+      .trim()
+      .isLength(2)
+      .withMessage('Password length short, min 2 char required'),
+    body('password2').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Password do not match')
+      }
+      return true
+    }),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        errors.array().forEach((error) => {
+          req.flash('error', error.msg)
+        })
+        res.render('register', { email: req.body.email, messages: req.flash() })
+        return
+      }
 
-    if (doesUserExist) {
-      res.redirect('/auth/register')
-      return
+      const { email } = req.body
+      const doesUserExist = await User.findOne({ email })
+
+      if (doesUserExist) {
+        res.redirect('/auth/register')
+        return
+      }
+
+      const user = new User(req.body)
+      await user.save()
+
+      req.flash('success', `${user.email} registered successfully!`)
+      res.redirect('/auth/login')
+    } catch (e) {
+      next(e)
     }
-
-    const user = new User(req.body)
-    await user.save()
-
-    res.send(user)
-  } catch (e) {
-    next(e)
   }
-})
+)
 
 module.exports = router
