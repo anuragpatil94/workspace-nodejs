dae73f4b70849828ec20b995881155565eaa307c
Anurag Patil <aspanurag@gmail.com>
2022-02-13 11:07:35 -0500

    Add Admin Routes | Role Authorization Middleware

--- rbac/routes/index.js
+++ rbac/routes/index.js
@@ -1,10 +1,17 @@
 const connectEnsureLogin = require('connect-ensure-login')
+const { roles } = require('../utils/constants')
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   res.render('index')
 })
 
+router.use(
+  '/admin',
+  connectEnsureLogin.ensureLoggedIn({ redirectTo: '/auth/login' }),
+  ensureAdmin,
+  require('./admin.route')
+)
 router.use('/auth', require('./auth.route'))
 router.use(
   '/user',
@@ -13,5 +20,23 @@ router.use(
   }),
   require('./user.route')
 )
+
+function ensureAdmin(req, res, next) {
+  if (req.user.role === roles.admin) {
+    next()
+  } else {
+    req.flash('warning', 'you are not Authorized to see this route')
+    res.redirect('/')
+  }
+}
+
+function ensureModerator(req, res, next) {
+  if (req.user.role === roles.moderator) {
+    next()
+  } else {
+    req.flash('warning', 'you are not Authorized to see this route')
+    res.redirect('/')
+  }
+}
 
 module.exports = router
