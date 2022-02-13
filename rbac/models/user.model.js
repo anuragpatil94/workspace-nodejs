6b200dfb33168fac3bf363af31e8c0b014e69054
Anurag Patil <aspanurag@gmail.com>
2022-02-13 11:07:10 -0500

    Add Roles to user Model| Check for Admin

--- rbac/models/user.model.js
+++ rbac/models/user.model.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const createHttpError = require('http-errors')
+const { roles } = require('../utils/constants')
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: Object.values(roles),
+    default: roles.client,
+  },
 })
 
 UserSchema.pre('save', async function (next) {
@@ -13,6 +19,11 @@ UserSchema.pre('save', async function (next) {
       const salt = await bcrypt.genSalt(10)
       const hashedPassword = await bcrypt.hash(this.password, salt)
       this.password = hashedPassword
+
+      // Check for Admin
+      if (this.email == process.env.ADMIN_EMAIL.toLowerCase()) {
+        this.role = roles.admin
+      }
     }
 
     next()
